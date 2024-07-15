7fef473e32d679a07e428ae491f9f6c8c60456be
shaimaayosef <shimaa.sayed8888@gmail.com>
2024-07-16 00:14:00 +0300

    email verification using mailhog

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->middleware('verified')->name('home');
